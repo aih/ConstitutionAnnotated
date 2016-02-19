9f02e3799f141f83840db714ea30c6d740fcc2fe
aih <aih@tabulaw.com>
2016-02-19 11:31:49 -0800

    Update to get keywords from elasticsearch (simple tf-idf algorithm)

--- pdfconvert.py
+++ pdfconvert.py
@@ -1,17 +1,19 @@
 #python
 # -*- coding: utf-8 -*-
+from __future__ import division
 from io import BytesIO
+import re
+import operator
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter, HTMLConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 
-import re
-
 from elasticsearch import Elasticsearch
-
 es = Elasticsearch()
 
+INDEX = 'constitution'
+DOCTYPE = 'page'
 
 def convert(page, html=None):
 
@@ -42,7 +44,7 @@ def getPageNumber(text):
     return pagenumber
 
 def savePage(pageindex, pagenumber, text, html):
-    es.index(index="constitution", doc_type="page", body={"pageindex":pageindex, "pagenumber":pagenumber, "text":text, 'html':html})
+    es.index(index=INDEX, doc_type=DOCTYPE, body={"pageindex":pageindex, "pagenumber":pagenumber, "text":text, 'html':html, 'keywords':[]})
 
 def convertPages(fname, pages = None):
     if not pages:
@@ -62,4 +64,28 @@ def convertPages(fname, pages = None):
 
         print(pagenumber)
     infile.close()
+
+def getBestTerms(terms,maxTerms=5):
+    termScores={term:terms[term]['doc_freq']/terms[term]['ttf'] for term in terms if len(term)>3 and (not term.isdigit()) and terms[term]['doc_freq']>1 and terms[term]['doc_freq']/terms[term]['ttf']<1}
+    sortedTerms = sorted(termScores.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedTerms[0:maxTerms]
+
+
+
+def setAllTerms(maxTerms=5):
+    numdocs = es.count(index=INDEX, doc_type=DOCTYPE)['count']
+    for pageindex in range(numdocs):
+        doc = es.search(index=INDEX, doc_type=DOCTYPE, body={"query": {"match":{"pageindex":pageindex}}})
+        docID=doc['hits']['hits'][0]['_id']
+        termvector = es.termvector(index=INDEX, doc_type=DOCTYPE, id=docID, body={"fields":["text"],"offsets":"false","positions":"false","term_statistics":"true","field_statistics":"true"})
+        try:
+            terms = termvector['term_vectors']['text']['terms']
+            bestTerms = getBestTerms(terms, maxTerms)
+            print(pageindex)
+            print(bestTerms)
+            es.update(index=INDEX, doc_type=DOCTYPE, id=docID, body={"doc": {"keywords":bestTerms}})
+        except:
+            pass
+
+
 
